6ab946112fa79efcd30dfd68fe8c23d2c28a8d89
Hamid <hapsa10@gmail.com>
2020-06-29 15:27:36 -0400

    Avances mejora reportes

--- src/features/dashboard/pages/reportes/detalle/reporte_historico_comisiones.js
+++ src/features/dashboard/pages/reportes/detalle/reporte_historico_comisiones.js
@@ -3,18 +3,21 @@ import format from 'date-fns/format'
 import NumberFormat from 'react-number-format';
 
 const ReporteHistoricoComisiones = ({data}) => {
+    var group1Count = 0;
+    var group2Count = 0;
     return data && data.length > 0 ?
         data.map((item,index) => {
             if( item.tipoRegistro === 1 ) {
+                group1Count++;
+                group2Count = 0;
                 return (
                     <Fragment key={`report_row_${index}`}>
                         <br/>
-                        <div style={{ marginTop: "4px" }}></div>
+                        <div style={{ marginTop: group1Count === 1 ? "0px" : "20px" }}></div>
                         <table className="footable table table-stripped footable-loaded default" style={{ borderCollapse: "collapse", marginBottom: "0.4rem" }}>
                             <tbody>
                                 <tr>
-                                <td width="110" align="left" style={{ border: "none", padding: "2px 8px" }}><b>Aseguradora</b> </td>
-                                    <td style={{ border: "none", padding: "0px 8px" }}>{item.aseguradora}</td>
+                                    <td className="rpt-group-1-title" width="110" align="left" style={{ border: "none", padding: "2px 8px" }}><b>Aseguradora: {item.aseguradora}</b> </td>
                                 </tr>
                             </tbody>
                         </table>
