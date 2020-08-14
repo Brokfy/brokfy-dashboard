12643f602f61deb7b176afd5961110b2d2292fa6
Vianney Gutierrez <vgutierrez@geovictoria.com>
2020-08-14 05:19:14 -0400

    acceso restrigido via url

--- src/features/dashboard/protected.js
+++ src/features/dashboard/protected.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useIsAuthenticated } from '../common/redux/hooks';
 import PageProtected from '../common/PageProtected';
+import { useLocation } from 'react-router-dom'
+import { useFetchRestricciones } from './redux/fetchRestricciones';
 
 const Protected = (props) => {
-  const { isAuthenticated } = useIsAuthenticated();
 
-  if ( isAuthenticated ) return props.children;
+  const { isAuthenticated } = useIsAuthenticated();
+  const { restricciones } = useFetchRestricciones();
+  const location = useLocation();
+
+  console.log({ restricciones, location });
+
+  if(!restricciones) 
+    return props.children;
+
+  if (restricciones.length > 0 && restricciones.filter(x => x.ruta === location.pathname).length > 0)
+    return <PageProtected />;
+
+  if (isAuthenticated)
+    return props.children;
 
   return <PageProtected />;
 }
